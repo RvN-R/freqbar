ffd02f8c4b16912c64a35ef7000fbe5ffb78e63c
Richard Van Nairn <rvannairn@googlemail.com>
2022-05-18 10:41:09 +0100

    created web audio api context and connected oscillator to button 2

--- scripts/apps.js
+++ scripts/apps.js
@@ -3,6 +3,8 @@ const button1 = document.getElementById('button1');
 let audio1 = new Audio();
 audio1.src = 'DANA.wav';
 
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+console.log(audioCtx)
 
 button1.addEventListener('click', function(){
     audio1.play();
@@ -12,5 +14,16 @@ button1.addEventListener('click', function(){
     audio1.addEventListener('ended', function(){
         console.log('Audio 1 ended!');
     });
-})
+});
 
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', playSound);
+function playSound(){
+    const oscillator = audioCtx.createOscillator();
+    oscillator.connect(audioCtx.destination);
+    oscillator.type = 'sine';
+    oscillator.start();
+    setTimeout(function (){
+        oscillator.stop();
+    }, 200);
+}
